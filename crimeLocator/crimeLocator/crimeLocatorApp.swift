bf8352e07dc46926742b7b74f5a5bb3415523d37
jainam-doshi <s3825891@student.rmit.edu.au>
2023-08-07 15:08:29 +1000

    Initial xCode Project

---
+++ crimeLocator/crimeLocator/crimeLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  crimeLocatorApp.swift
+//  crimeLocator
+//
+//  Created by Jainam Doshi on 7/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct crimeLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
